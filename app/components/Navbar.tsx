0a9c6eab2c37711db4b323561a521c02e820d76d
asadali22 <leoali851@gmail.com>
2026-05-06 13:22:45 +0500

    fix: iOS scroll permanently blocked on mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -37,8 +37,17 @@ export default function Navbar() {
   useEffect(() => { setOpen(false); }, [pathname]);
 
   useEffect(() => {
-    document.body.style.overflow = open ? 'hidden' : '';
-    return () => { document.body.style.overflow = ''; };
+    if (open) {
+      document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
+    };
   }, [open]);
 
   return (
